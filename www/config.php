1a0545b0e348d67d31e7c2b855642b19bd196044
oleggood22 <oleggood22@gmail.com>
2019-03-20 20:45:26 +0300

    342342131

---
+++ www/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '5f7edac0'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '4420ebe6812e560bba996629d935bc613034281c2a95753980b9bc0de69c5b7e254f3a7938cea33631ff8'); //Ключ доступа сообщества
